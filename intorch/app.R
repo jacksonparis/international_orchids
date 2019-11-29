8482cde4bb0be5c2efab810c3231e259268d3945
Jackson Kehoe <jacksonkehoe@Jacksons-MBP.home>
2019-11-28 19:24:54 -0500

    Added a map tab.

--- intorch/app.R
+++ intorch/app.R
@@ -21,20 +21,25 @@ aggregated <- read_csv("aggregated.csv", col_types =
                                subpopulation_name = col_logical()
                            ))
 
+points_plus <- read_csv("points_plus.csv", col_types = 
+                            cols(
+                                binomial = col_character(),
+                                longitude = col_double(),
+                                latitude = col_double(),
+                                redlistCategory = col_character(),
+                                label = col_character())) 
+
 # Making the user interface
 ui <- navbarPage("Orchids Around the World",
                  tabPanel("Importers",
                           # Putting the reactive input into a sidebar
                           sidebarLayout(
                               sidebarPanel(
-                                  # Year input
+                                  # Text
                                   helpText("Check out some numbers on 2017's 
                     top importers of orchids globally. 
                     All trade data is sourced from CITES."),
-                                  selectInput("year", "Select a Year", c(1995, 1996, 1997, 1998, 1999, 2000,
-                                                                         2001, 2002, 2003, 2004, 2005, 2006,
-                                                                         2007, 2008, 2009, 2010, 2011, 2012, 
-                                                                         2013, 2014, 2015, 2016, 2017)),
+                                  
                                   # Country input
                                   selectInput("country", "Select a Country", 
                                               choices = c(
@@ -48,7 +53,13 @@ ui <- navbarPage("Orchids Around the World",
                                                   "France",
                                                   "Netherlands",
                                                   "Singapore"
-                                              ))),
+                                              )),
+                                  
+                                  # Year Input
+                                  selectInput("year", "Select a Year", c(1995, 1996, 1997, 1998, 1999, 2000,
+                                                                         2001, 2002, 2003, 2004, 2005, 2006,
+                                                                         2007, 2008, 2009, 2010, 2011, 2012, 
+                                                                         2013, 2014, 2015, 2016, 2017))),
                               # Making space for the table output
                               mainPanel(
                                   tableOutput("table")
@@ -56,40 +67,60 @@ ui <- navbarPage("Orchids Around the World",
                           )
                  ), 
                  tabPanel("Map",
-                          p("This tab will display a map
-             of endangered orchid distributions."),
-                          verbatimTextOutput("Map"))
+                          leafletOutput("map")
 )
-
-
+)
 
 # Setting up the server function
 server <- function(input, output) {
     # Making the table output
     output$table <- renderTable({
         top_ten <- 
+            
             # Starting with the entire dataset
             aggregated %>%
+            
             # Filtering by importer country, year, and live plants
             filter(importer_country == as.character(input$country), 
                    term == "live", year == input$year) %>%
+            
             # Group by exporter country
             group_by(exporter_country) %>%
+            
             # Replacing NA with zero so summing can happen later
             replace_na(list(importer_reported_quantity = 0)) %>%
+            
             # Creating new columns, plant and import counts
             summarize(importer_count = sum(importer_reported_quantity),
                       number_imports = n()) %>%
+            
             # Arrange and head to filter top 10
             arrange(desc(number_imports)) %>%
             head(top_ten, n = 10) %>%
+            
             # Select columns to be in final table display
             select(exporter_country, importer_count, number_imports)
+        
         # Renaming the columns
         names(top_ten) <- c("Exporter", 
-                            "Importer Reported Quantity", 
-                            "Number of Imports")
+                            "Number of Live Specimens", 
+                            "Number of Import Shipments")
         top_ten
+    })
+    
+    output$map <- renderLeaflet({
+        leaflet() %>%
+            addProviderTiles(providers$Esri.OceanBasemap) %>%
+            setView(lng = 0, lat = 20, zoom = 1.5) %>% 
+            addCircleMarkers(data = points_plus, ~longitude, ~latitude,
+                             weight = 0.5,
+                             col = ~palette(redlistCategory), 
+                             fillColor = ~palette(redlistCategory),
+                             radius = 3, 
+                             fillOpacity = 0.9, 
+                             stroke = T, 
+                             label = ~points_plus$label
+            )
     })
 }
 
