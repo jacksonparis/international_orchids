2162f4643ef4b739a626e9dadb187d7cb002c3b3
Jackson Kehoe <jacksonkehoe@dhcp-10-250-24-14.harvard.edu>
2019-12-14 00:09:46 -0500

    Final commits

--- intorch/app.R
+++ intorch/app.R
@@ -1,6 +1,12 @@
 library(shiny)
 library(tidyverse)
 library(markdown)
+library(leaflet)
+library(shinythemes)
+library(wordcloud2)
+library(slam)
+library(tm)
+library(htmltools)
 
 aggregated <- read_csv("aggregated.csv", col_types = 
                            cols(
@@ -34,6 +40,36 @@ im_index <- aggregated %>%
            threats, range, use_trade) %>%
     mutate(binomial = str_c(genus, species, sep = "-"))
 
+create_wordcloud <- function(data, num_words = 100, background = "white") {
+    # Function found on Datacamp:
+    # Building Web Application in R with Shiny: Case Studies
+    
+    # If text is provided, convert it to a dataframe of word frequencies
+    if (is.character(data)) {
+        corpus <- Corpus(VectorSource(data))
+        corpus <- tm_map(corpus, tolower)
+        corpus <- tm_map(corpus, removePunctuation)
+        corpus <- tm_map(corpus, removeNumbers)
+        corpus <- tm_map(corpus, removeWords, stopwords("english"))
+        tdm <- as.matrix(TermDocumentMatrix(corpus))
+        data <- sort(rowSums(tdm), decreasing = TRUE)
+        data <- data.frame(word = names(data), freq = as.numeric(data))
+    }
+    
+    # Make sure a proper num_words is provided
+    if (!is.numeric(num_words) || num_words < 3) {
+        num_words <- 3
+    }  
+    
+    # Grab the top n most common words
+    data <- head(data, n = num_words)
+    if (nrow(data) == 0) {
+        return(NULL)
+    }
+    
+    wordcloud2(data, backgroundColor = background)
+}
+
 library(rvest)
 library(imager)
 library(magick)
@@ -44,41 +80,29 @@ search_word <- function() {
     sample_n(im_index, size = 1) -> random
     
     name <- as.character(random[1,8])
-    
-    keyword <- str_c("https://www.google.com/search?site=&tbm=isch&q=", 
-                     name,
-                     sep = "")
-    
-    search <- read_html(keyword)
-    
-    #Grab all <img> tags, get their "src" attribute, a URL to an image
-    urls <- search %>%
-        html_nodes("img") %>% 
-        html_attr("src") 
-    #Get urls of pictures
-    
-    ima <- image_read(urls[1]) %>%
-        image_scale(geometry = "100")
-    
-    ima <- image_border(image_background(ima, "hotpink"), "#000080", "5x5")
-    
-    ima
-    
 }
 
-
 # Making the user interface
-ui <- navbarPage("Orchids Around the World",
-                 tabPanel("Importers",
-                          # Putting the reactive input into a sidebar
+ui <- navbarPage(theme = shinytheme("cerulean"),
+"Orchids Around the World",
+                 tabPanel("About",
+                     htmlOutput("about1"),
+                     actionButton("button", "Random Orchid"),
+                     imageOutput("image"),
+                     htmlOutput("about2"),
+                     p("Link to informative video about the project: 
+                           https://www.youtube.com/watch?v=tlUXSMwKek8")
+                 ),
+                 tabPanel("Importers 1",
                           sidebarLayout(
                               sidebarPanel(
-                                  # Text
-                                  helpText("Check out some numbers on 2017's 
-                    top importers of orchids globally. 
-                    All trade data is sourced from CITES."),
+                                  helpText("Ten countries imported the most
+                                           orchids worldwide in 2017. This 
+                                           interactive allows you to see where
+                                           those same countries have imported
+                                           orchids from, all the way back 
+                                           to 1995."),
                                   
-                                  # Country input
                                   selectInput("country", "Select a Country", 
                                               choices = c(
                                                   "United States",
@@ -102,79 +126,121 @@ ui <- navbarPage("Orchids Around the World",
                               mainPanel(
                                   tableOutput("table"),
                                   plotOutput("plot")
-                              )
                           )
-                 ), 
+                 )),
+                tabPanel("Importers 2",
+                         sidebarLayout(
+                             sidebarPanel(
+                                 helpText("The slope is based on a model representing
+                                          change in number of live specimens imported 
+                                          or exported per year for each country. Greater
+                                          slopes correspond to higher levels of activity
+                                          over the past fifteen years."),
+                                 
+                                 selectInput("side", "View Countries as:",
+                                             c("importer_country", 
+                                               "exporter_country"))
+                             ),
+                             mainPanel(
+                                 plotOutput("plot2")
+                             )
+                         )),
                  tabPanel("Map",
-                          leafletOutput("map")
-),
-                 tabPanel("Explore Species",
-                          sidebarLayout(
-                              sidebarPanel(
-                                  helpText("Click to discover something about
-                                           a random orchid in the IUCN 
-                                           database."),
-                                  actionButton("button", "Random Orchid"),
-                              ),
-                          mainPanel(
-                          imageOutput("image"),
-                          textOutput("text"))
-                          ))
-)
+                          leafletOutput("map", width = "75%", height = "500px"),
+                          htmlOutput("text2")),
+                tabPanel("Threats",
+                         sidebarLayout(
+                             sidebarPanel(
+                                 selectInput("category", 
+                                             "Select a Redlist category", choices = c(
+                                                                                "Critically Endangered",
+                                                                                "Endangered",
+                                                                                "Vulnerable",
+                                                                                "Near Threatened",
+                                                                                "Least Concern"))),
+                
+                             mainPanel(
+                                 wordcloud2Output(outputId = "cloud"),
+                                 htmlOutput("text3")
+                             )
+                        
+                                     )
+
+            )
+             
+         )
+
+
 
 # Setting up the server function
-server <- function(input, output) {
+server <- function(input, output, session) {
     # Making the table output
     output$table <- renderTable({
         top_ten <- 
-            
-            # Starting with the entire dataset
             aggregated %>%
-            
-            # Filtering by importer country, year, and live plants
             filter(importer_country == as.character(input$country), 
                    term == "live", year == input$year) %>%
-            
-            # Group by exporter country
             group_by(exporter_country) %>%
-            
-            # Replacing NA with zero so summing can happen later
             replace_na(list(importer_reported_quantity = 0)) %>%
-            
-            # Creating new columns, plant and import counts
+            replace_na(list(exporter_reported_quantity = 0)) %>%
             summarize(importer_count = sum(importer_reported_quantity),
+                      exporter_count = sum(exporter_reported_quantity),
                       number_imports = n()) %>%
-            
-            # Arrange and head to filter top 10
             arrange(desc(number_imports)) %>%
             head(top_ten, n = 10) %>%
-            
-            # Select columns to be in final table display
-            select(exporter_country, importer_count, number_imports)
-        
-        # Renaming the columns
+            select(exporter_country, importer_count, exporter_count, 
+                   number_imports)
+
         names(top_ten) <- c("Exporter", 
-                            "Number of Live Specimens", 
+                            "Importer Reported Specimens",
+                            "Exporter Reported Specimens",
                             "Number of Import Shipments")
         top_ten
     })
     
     output$plot <- renderPlot({
-        top_ten <- 
+        top_ten <-
             aggregated %>%
             filter(importer_country == as.character(input$country), 
                    term == "live", year == input$year) %>%
             group_by(exporter_country) %>%
             replace_na(list(importer_reported_quantity = 0)) %>%
+            replace_na(list(exporter_reported_quantity = 0)) %>%
             summarize(importer_count = sum(importer_reported_quantity),
-                      number_imports = n()) %>%
+                  exporter_count = sum(exporter_reported_quantity),
+                  number_imports = n()) %>%
             arrange(desc(number_imports)) %>%
             head(top_ten, n = 10) %>%
-            select(exporter_country, importer_count, number_imports)
-
+            select(exporter_country, importer_count, exporter_count)
+        
+        top_ten_graph <- pivot_longer(top_ten, 
+                                      cols = c(importer_count, exporter_count),
+                                      values_to = "value")
+        
+        ggplot(top_ten_graph, aes(x = exporter_country, y = value, 
+                                  fill = name)) +
+            geom_bar(position = "dodge", stat = "identity") + 
+            theme(axis.text.x = element_text(angle = 55, hjust = 1),
+                  legend.title = element_blank()) + 
+            labs(x = "Exporter Coutnry", 
+                 y = "Reported Live Specimens")
+ 
     })
     
     output$map <- renderLeaflet({
+        points_plus$redlistCategory <- 
+            factor(points_plus$redlistCategory,
+                   levels = c(
+                       "Extinct",
+                       "Critically Endangered",
+                       "Endangered",
+                       "Vulnerable",
+                       "Near Threatened",
+                       "Least Concern"
+                   ))
+        
+        palette <- colorFactor("RdYlGn", points_plus$redlistCategory)
+        
         leaflet() %>%
             addProviderTiles(providers$Esri.OceanBasemap) %>%
             setView(lng = 0, lat = 20, zoom = 1.5) %>% 
@@ -189,12 +255,27 @@ server <- function(input, output) {
             )
     })
     
-    ima <- eventReactive(input$button, {
+    name <- eventReactive(input$button, {
         search_word() })
     
     output$image <- renderImage({
         
-        ima <- image_write(ima(), path = "ima.jpeg", format = "jpeg")
+        keyword <- str_c("https://www.google.com/search?site=&tbm=isch&q=",
+                         name(),
+                         sep = "")
+        
+        search <- read_html(keyword)
+        
+        urls <- search %>%
+            html_nodes("img") %>%
+            html_attr("src")
+        
+        ima <- image_read(urls[1]) %>%
+            image_scale(geometry = "100")
+        
+        ima <- image_border(image_background(ima, "hotpink"), "#000080", "5x5")
+        
+        ima <- image_write(ima, path = "ima.jpeg", format = "jpeg")
         
             list(src = "ima.jpeg",
                  contentType = "image",
@@ -203,7 +284,150 @@ server <- function(input, output) {
             )}, 
             
             deleteFile = TRUE)
+    
+    output$text2 <- renderUI({
+        HTML("This map was compiled using data from the IUCN Species Redlist
+             database. 
+             <br/>
+             The Redlist is “the world’s most comprehensive information 
+             source on the global conservation status of animal, fungus 
+             and plant species.”
+             <br/>
+             Each point represents a wild orchid assessed during a population
+             study. Hover over the dots to see the species names, status and 
+             year observed.
+             <br/>
+             <i>https://www.iucnredlist.org</i>"
+             )   
+    })
+    
+    output$about1 <- renderUI({
+        HTML("<b> So... what are orchids? </b>
+             <br/><br/>
+             A diverse family of plants, Orchidaceae, with a global distribution 
+             -- they are found in the wild on every continent except Antarctica. 
+             Their vast array of floral forms has captured the attention
+             of insect pollinators and biologists alike for many years. 
+             <br/><br/>
+             <b> Did you know... </b>
+             <br/><br/>
+             There are almost 30,000 species of orchids -- and many, many
+             more hybrids of these used in ornamental horticulture. Besides 
+             the flowers, other parts of certain species are used
+             as medicine or food. Vanilla beans are also dried orchid fruits.
+             <br/><br/>
+             Click the button to generate a picture of a random orchid from 
+             the IUCN species database, or scroll down to read more about the
+             data.")
+    })
+    
+    output$about2 <- renderUI({
+        HTML("<b> About the Data </b>
+              <br/><br/>
+        CITES is “…an international agreement between governments. 
+        Its aim is to ensure that international trade in specimens of 
+        wild animals and plants does not threaten their survival.”
+        <br/><br/>
+        The organization provides a list of species that are to be monitored, 
+        each placed in one of three “appendices” in relation 
+        to its threat level or protection status.
+        <br/><br/>
+        As you can see in the table, the importer and exporter 
+        accounts often do not match up. CITES acknowledges these 
+        inconsistencies in their guide to using trade data:
+        <br/><br/>
+        “It should be noted that the details of a particular 
+        transaction reported by both the exporter/re-exporter and 
+        importer frequently fail to show perfect correlation, 
+        and therefore will not appear in the same line of the 
+        comparative tabulation. This is often for one or more of 
+        the following reasons:
+        <br/>
+        -The source of the items and purpose of the transaction are 
+         often reported differently, if at all;
+        <br/>
+        -Terms and units may also be reported differently for the same 
+         items in trade;
+        <br/>
+        -One of the trading partners may not have submitted a report 
+         for the year in question, or may not be a CITES Party;
+        <br/>
+        -Specimens may be exported at the end of one year but not 
+         received by the importer until the following year;
+         <br/>
+        -Trade may be reported at species level by one country 
+         and at a higher taxonomic level by another. This is 
+         particularly common in the reporting of artificially 
+         propagated plants.”
+         <br/><br/>
+         CITES does not represent international law —  
+         “it provides a framework to be respected by each Party 
+         [participating country], which has to adopt its own domestic 
+         legislation to ensure that CITES is implemented at the national 
+         level.”
+        <br/><br/>
+             <i>https://www.cites.org/eng</i>
+             <br/><br/>
+             Project inspired in part by Hinsley et. al 2017, 
+             <i>A review of the trade in orchids and its implications for conservation<i/>
+             <br/><br/>
+             ")
+    })
+    
+    output$cloud <- renderWordcloud2({
+        chunk <- 
+            aggregated %>%
+            select(genus, species, rationale, redlist_category, year) %>%
+            filter(year == 2017) %>%
+            filter(redlist_category == input$category) %>%
+            distinct(rationale) %>%
+            as.character() %>%
+            htmlEscape() 
+        
+        create_wordcloud(chunk, num_words = 100)
+    })
+    
+    output$text3 <- renderUI({
+        HTML("<br/><br/>
+        <p>These are some of the most common words in the <i>rationale</i>
+        tabs for IUCN listed species. This tab contains information on 
+             why the species was placed in its current Redlist category.</p>
+             <br/>
+             A sample:
+             <br/>
+             <i><p>Due to the small number of locations and ongoing habitat decline, 
+             we assess this taxon as Endangered. (Habenaria isoantha) <p/>
+             </i>")
+    })
+    
+    output$plot2 <- renderPlot({
+        model2data <- 
+            aggregated %>%
+            select(year, importer_country, exporter_country,
+                   exporter_reported_quantity) %>%
+            drop_na() %>%
+            filter(year >= 1995) %>%
+            group_by(!! rlang::sym(input$side), year) %>%
+            mutate(sum = sum(exporter_reported_quantity)) %>%
+            mutate(number = n()) %>%
+            filter(number >= 15) %>%
+            ungroup() %>%
+            group_by(!! rlang::sym(input$side)) %>%
+            nest() %>%
+            mutate(models = map(data, ~lm(sum ~ year, data = .x))) %>%
+            mutate(coefs = map(models, ~coef(.x))) %>%
+            mutate(intercept = map_dbl(coefs, ~purrr::pluck(.x, "(Intercept)"))) %>%
+            mutate(year = map_dbl(coefs, ~purrr::pluck(.x, "year"))) %>%
+            arrange(desc(year)) %>%
+            drop_na()
+        
+        ggplot(model2data, aes(x = reorder(!! rlang::sym(input$side), year), 
+                               y = year)) + 
+            geom_col() +
+            coord_flip() + 
+            labs(y = "Slope")
+    })
+    
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
