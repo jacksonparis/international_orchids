1a71bd532baf9ecd7f59462457affc631915703b
Jackson Kehoe <jacksonkehoe@dhcp-10-250-24-14.harvard.edu>
2019-12-13 23:07:09 -0500

    Final Commits for these things

--- intnatl_orchids.prep.R
+++ intnatl_orchids.prep.R
@@ -1,4 +1,5 @@
 library(janitor)
+library(broom)
 
 # Writing a csv file containing all of the data I have aggregated
 dir.create("./data")
@@ -56,4 +57,113 @@ as.character(top_ten$importer_count)
 top_ten <- top_ten %>% 
   str_replace_all("0", "Not recorded") %>%
   tbl(top_ten)
+
+library(devtools)
+install_github(install_github("bwlewis/rthreejs"))
+
+
+top_ten <-
+  aggregated %>%
+  filter(importer_country == "United States", 
+         term == "live", year == 2000) %>%
+  group_by(exporter_country) %>%
+  replace_na(list(importer_reported_quantity = 0)) %>%
+  replace_na(list(exporter_reported_quantity = 0)) %>%
+  summarize(importer_count = sum(importer_reported_quantity),
+            exporter_count = sum(exporter_reported_quantity),
+            number_imports = n()) %>%
+  arrange(desc(number_imports)) %>%
+  head(top_ten, n = 10) %>%
+  select(exporter_country, importer_count, exporter_count)
+
+top_ten_graph <- pivot_longer(top_ten, 
+                              cols = c(importer_count, exporter_count),
+                              values_to = "value")
+
+chunk <- 
+  aggregated %>%
+  select(genus, species, rationale, redlist_category, year) %>%
+  filter(year == 2017) %>%
+  filter(redlist_category == "Endangered") %>%
+  distinct(rationale) %>%
+  as.character()
+
+# Cleaning data for model
+model_data <- 
+  aggregated %>%
+  select(redlist_category, realm, year) %>%
+  filter(! redlist_category %in% c("Lower Risk/near threatened", 
+                                   "Data Deficient")) %>%
+  drop_na()
+
+# Renaming the characters
+model_data$redlist_category[model_data$redlist_category=="Least Concern"] <- "1"
+model_data$redlist_category[model_data$redlist_category=="Near Threatened"] <- "2"
+model_data$redlist_category[model_data$redlist_category=="Vulnerable"] <- "3"
+model_data$redlist_category[model_data$redlist_category=="Endangered"] <- "4"
+model_data$redlist_category[model_data$redlist_category=="Critically Endangered"] <- "5"
+
+model_data <-
+  model_data %>%
+  transform(redlist_category = as.numeric(redlist_category)) %>%
+  filter(! realm %in% c("Afrotropical|Palearctic", 
+                        "Australasian|Indomalayan",
+                        "Australasian|Oceanian|Palearctic",
+                        "Indomalayan|Palearctic",
+                        "Nearctic|Neotropical",
+                        "Afrotropical|Nearctic|Palearctic",
+                        "Australasian|Indomalayan|Palearctic",
+                        "Nearctic|Palearctic"))
+
+model1 <- lm(redlist_category ~ realm, model_data)
+
+model_data$realm <- factor(model_data$realm, 
+                           levels = c("Australasian",
+                                      "Nearctic",
+                                      "Neotropical",
+                                      "Oceanian",
+                                      "Palearctic",
+                                      "Indomalayan",
+                                      "Afrotropical"))
+
+realmsplot <- ggplot(model_data, 
+                     aes(x = realm, y = redlist_category, color = realm)) + 
+  geom_jitter(width = .01, alpha = .05) +
+  theme(axis.text.x = element_text(angle = 55, hjust = 1),
+        legend.position = "none") + 
+  labs(x = "Realm", y = "Least to Highest Concern")
+
+model2data <- 
+  aggregated %>%
+  select(year, importer_country, exporter_country,
+         exporter_reported_quantity) %>%
+  drop_na() %>%
+  filter(year >= 1995) %>%
+  group_by(importer_country, year) %>%
+  mutate(sum = sum(exporter_reported_quantity)) %>%
+  mutate(number = n()) %>%
+  filter(number >= 15) %>%
+  ungroup() %>%
+  group_by(importer_country) %>%
+  nest() %>%
+  mutate(models = map(data, ~lm(sum ~ year, data = .x))) %>%
+  mutate(coefs = map(models, ~coef(.x))) %>%
+  mutate(intercept = map_dbl(coefs, ~purrr::pluck(.x, "(Intercept)"))) %>%
+  mutate(year = map_dbl(coefs, ~purrr::pluck(.x, "year"))) %>%
+  arrange(desc(year)) %>%
+  drop_na()
+
+# Change in US exports over time: countries and numbers
+# Change in US imports over time
+
+ggplot(model2data, aes(x = reorder(importer_country, year), 
+                       y = year)) + 
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 55, hjust = 1)) + 
+  coord_flip() + 
+  labs(x = "Shipped To", y = "Slope")
+
+
+
+
 
--- shiny.prep.r
+++ shiny.prep.r
@@ -20,3 +20,47 @@ aggregated <- read_csv("intorch/aggregated.csv", col_types =
                            infra_authority = col_logical(),
                            subpopulation_name = col_logical()
                          ))
+
+write.csv(points_plus, "./intorch/points_plus.csv", row.names = FALSE)
+
+points_plus <- read_csv("./intorch/points_plus.csv", col_types = 
+                          cols(
+                            binomial = col_character(),
+                            longitude = col_double(),
+                            latitude = col_double(),
+                            redlistCategory = col_character(),
+                            label = col_character())) 
+
+
+search_word <- function() {
+  
+  sample_n(im_index, size = 1) -> random
+  
+# //// Cap here for the eventReactive
+  
+# Display this text: 
+  name <- as.character(random[1,8])
+  
+# Image search: if possible do this in output image, 
+# although I'm not sure if that will work...
+  keyword <- str_c("https://www.google.com/search?site=&tbm=isch&q=", 
+                   name,
+                   sep = "")
+  
+  search <- read_html(keyword)
+  
+  #Grab all <img> tags, get their "src" attribute, a URL to an image
+  urls <- search %>%
+    html_nodes("img") %>% 
+    html_attr("src") 
+  #Get urls of pictures
+  
+  ima <- image_read(urls[1]) %>%
+    image_scale(geometry = "100")
+  
+  ima <- image_border(image_background(ima, "hotpink"), "#000080", "5x5")
+  
+  ima
+  
+}
+
